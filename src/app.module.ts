edd352ebf4e38476741a0407508b15dffe2595d8
Salik-07 <salikhussain41@gmail.com>
2024-08-27 16:15:55 +0500

    Add Reports entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { Users } from './users/users.entity';
+import { Reports } from './reports/reports.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [Users],
+      entities: [Users, Reports],
       synchronize: true,
     }),
     UsersModule,
