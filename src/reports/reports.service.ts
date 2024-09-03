b32962f65e42674c745bf8fbeb3e8f68bea94615
Salik-07 <salikhussain41@gmail.com>
2024-09-03 23:59:30 +0500

    Add reports service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Reports } from './reports.entity';
+import { CreateReportDTO } from './dtos/create-report.dto';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  create(reportDto: CreateReportDTO) {}
+}
